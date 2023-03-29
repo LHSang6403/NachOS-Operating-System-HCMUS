e10c9abf7347c5018f96e934cef5e3a2e4256521
HuyBean <nqhuy0711@gmail.com>
2023-03-29 11:58:40 +0700

    Commit again

--- userprog/exception.cc
+++ userprog/exception.cc
@@ -51,72 +51,21 @@
 //	are in machine.h.
 //----------------------------------------------------------------------
 
-<<<<<<< HEAD
 void Increase_PC()
-=======
-void inc_program_counter() // PrevPC = CurrPC; CurrPC = NextPC; NextPC + 4
->>>>>>> b234f5893828cf45b929ee61bccfef40c4febde9
 {
 	/* set previous programm counter (debugging only)
      * similar to: registers[PrevPCReg] = registers[PCReg];*/
-	machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg)); // PCReg: current Programm counter, then write this to the previous PC
+	machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
 
 	/* set programm counter to next instruction
      * similar to: registers[PCReg] = registers[NextPCReg]*/
-	machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg)); // get value of the next PC, then write it to the current PC
+	machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
 
 	/* set next programm counter for brach execution
      * similar to: registers[NextPCReg] = pcAfter;*/
-	machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg) + 4); // write the next 4 bytes to the next PC
+	machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg) + 4);
 }
 
-<<<<<<< HEAD
-=======
-char *User2System(int virtAddr, int limit)
-{
-	int i; // index
-	int oneChar;
-	char *kernelBuf = NULL;
-	kernelBuf = new char[limit + 1]; //need for terminal string
-	if (kernelBuf == NULL)
-		return kernelBuf;
-	memset(kernelBuf, 0, limit + 1);
-	//printf("\n Filename u2s:");
-	for (i = 0; i < limit; i++)
-	{
-		machine->ReadMem(virtAddr + i, 1, &oneChar);
-		kernelBuf[i] = (char)oneChar;
-		//printf("%c",kernelBuf[i]);
-		if (oneChar == 0)
-			break;
-	}
-	return kernelBuf;
-}
-//Ngược lại ta có hàm System2User như sau:
-
-// Input: - User space address (int)
-// - Limit of buffer (int)
-// - Buffer (char[])
-// Output:- Number of bytes copied (int)
-// Purpose: Copy buffer from System memory space to User memory space
-int System2User(int virtAddr, int len, char *buffer)
-{
-	if (len < 0)
-		return -1;
-	if (len == 0)
-		return len;
-	int i = 0;
-	int oneChar = 0;
-	do
-	{
-		oneChar = (int)buffer[i];
-		machine->WriteMem(virtAddr + i, 1, oneChar);
-		i++;
-	} while (i < len && oneChar != 0);
-	return i;
-}
-
->>>>>>> b234f5893828cf45b929ee61bccfef40c4febde9
 void ExceptionHandler(ExceptionType which)
 {
 	int type = machine->ReadRegister(2);
@@ -127,53 +76,10 @@ void ExceptionHandler(ExceptionType which)
 	case NoException:
 		return;
 
-    case PageFaultException:
-        DEBUG('a', "\n No valid translation found"); // debug
-        printf("\n\n No valid translation found"); // run time
-        interrupt->Halt(); // close os
-        break;
-
-    case ReadOnlyException:
-        DEBUG('a', "\n Write attempted to page marked \"read-only\"");
-        printf("\n\n Write attempted to page marked \"read-only\"");
-        interrupt->Halt();
-        break;
-
-    case BusErrorException:
-        DEBUG('a', "\n Translation resulted in an invalid physical address");
-        printf("\n\n Translation resulted in an  invalid physical address");
-        interrupt->Halt();
-        break;
-
-    case AddressErrorException:
-        DEBUG('a', "\n Unaligned reference or one that was beyond the end of the address space");
-        printf("\n\n Unaligned reference or one that was beyond the end of the address space");
-        interrupt->Halt();
-        break;
-
-    case OverflowException:
-        DEBUG('a', "\n Integer overflow in add or sub");
-        printf("\n\n Integer overflow in add or sub");
-        interrupt->Halt();
-        break;
-
-    case IllegalInstrException:
-        DEBUG('a', "\n Unimplemented or reserved instr");
-        printf("\n\n Unimplemented or reserved instr");
-        interrupt->Halt();
-        break;
-
-    case NumExceptionTypes:
-        DEBUG('a', "\n Num Exception Types");
-        printf("\n\n Num Exception Types");
-        interrupt->Halt();
-        break;
-
 	case SyscallException:
 	{
 		switch (type)
 		{
-<<<<<<< HEAD
 		case SC_Halt:
 		{
 			A.Syscall_Halt();
@@ -273,85 +179,5 @@ void ExceptionHandler(ExceptionType which)
 		ASSERT(FALSE);
 		break;
 	}
-=======
-			case SC_Halt:
-				DEBUG('a', "\n Shutdown, initiated by user program.");
-				printf("\n\n Shutdown, initiated by user program.");
-				interrupt->Halt();
-				break;
-			case SC_Print:
-			{
-				int virtAddr = machine->ReadRegister(4);
-				char *temp = new char[MaxLength];
-				//memset(temp, 0, MaxLength);
-				temp = User2System(virtAddr, MaxLength);
-				int i = 0;
-				while (temp[i] != 0)
-				{
-					synchConsole->Write(temp + i, 1);
-					i++;
-				}
-				temp[i] = '\n';
-				synchConsole->Write(temp + i, 1);
-				delete[] temp;
-				break;
-			}
-			case SC_Create:
-			{
-				int virtAddr;
-				char *filename;
-				DEBUG('a', "\n SC_Create call ...");
-				DEBUG('a', "\n Reading virtual address of filename");
-				// Lấy tham số tên tập tin từ thanh ghi r4
-				virtAddr = machine->ReadRegister(4);
-				DEBUG('a', "\n Reading filename.");
-				// MaxFileLength là = 32
-				filename = User2System(virtAddr, MaxFileLength + 1);
-				if (filename == NULL)
-				{
-					printf("\n Not enough memory in system");
-					DEBUG('a', "\n Not enough memory in system");
-					machine->WriteRegister(2, -1);
-					// trả về lỗi cho chương trình người dùng
-					delete filename;
-					return;
-				}
-				DEBUG('a', "\n Finish reading filename.");
-				//DEBUG('a',"\n File name : '"<<filename<<"'");
-				// Create file with size = 0
-				// Dùng đối tượng fileSystem của lớp OpenFile để tạo file,
-				// việc tạo file này là sử dụng các thủ tục tạo file của hệ điều
-				// hành Linux, chúng ta không quản ly trực tiếp các block trên
-				// đĩa cứng cấp phát cho file, việc quản ly các block của file
-				// trên ổ đĩa là một đồ án khác
-				if (!fileSystem->Create(filename, 0))
-				{
-					printf("\n Error create file '%s'", filename);
-					machine->WriteRegister(2, -1);
-					delete filename;
-					return;
-				}
-				machine->WriteRegister(2, 0);
-				// trả về cho chương trình người dùng thành công
-				delete filename;
-				break;
-			}
-			case SC_Scanf:
-			{
-				char *buf = new char[32];
-				if (buf == 0)
-					break;
-				int bufAddrUser = machine->ReadRegister(4);
-				int length = machine->ReadRegister(5);
-				int sz = synchConsole->Read(buf, length);
-				System2User(bufAddrUser, sz, buf);
-				delete[] buf;
-			}
-			default:
-				printf("\n Unexpected user mode exception (%d %d)", which, type);
-				interrupt->Halt();
-		}
-		inc_program_counter(); // increase PC after each syscall
->>>>>>> b234f5893828cf45b929ee61bccfef40c4febde9
 	}
 }
